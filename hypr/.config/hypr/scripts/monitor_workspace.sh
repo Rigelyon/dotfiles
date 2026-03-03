7ad063efbc9fcc0d4f483748c91c0c92e35f2f0d
Rigelyon <pandudwi120@gmail.com>
2026-03-04 01:58:23 +0700

    Cofigure waybar with its colorscheme

--- hypr/.config/hypr/scripts/monitor_workspace.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-RECORD_SCRIPT="$HOME/.config/hypr/scripts/switch_last_workspace.sh"
-
-socat -U - UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock | while read -r line; do
-    if [[ $line == "workspace>>"* ]]; then
-        $RECORD_SCRIPT
-    fi
-done
